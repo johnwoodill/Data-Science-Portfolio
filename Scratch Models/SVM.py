cdf59f7525483a96086c304b22822d4875f0d094
johnwoodill <johnwoodill@gmail.com>
2018-12-17 19:33:54 -0700

    commenting SVM scratch code

--- Scratch Models/SVM.py
+++ Scratch Models/SVM.py
@@ -15,80 +15,114 @@ class Support_Vector_Machine:
             self.ax = self.fig.add_subplot(1,1,1)
     # train
     def fit(self, data):
+        
+        # Get data
         self.data = data
+        
+        # Magnitude of w where [w, b]
         # { ||w||: [w,b] }
         opt_dict = {}
-
+        
+        # Apply to vector of w for each step
         transforms = [[1,1],
                       [-1,1],
                       [-1,-1],
                       [1,-1]]
-
+        
+        # Get max and min for x and b to step with
         all_data = []
-        for yi in self.data:
+        for yi in self.data:       # yi class
             for featureset in self.data[yi]:
                 for feature in featureset:
                     all_data.append(feature)
-
+        
+        # Assign min and max
         self.max_feature_value = max(all_data)
         self.min_feature_value = min(all_data)
         all_data = None
 
-        # support vectors yi(xi.w+b) = 1
+        # support vectors yi(xi.w+b) = 1 (best support vector)
         
-
+        # Big to small steps to learn on
         step_sizes = [self.max_feature_value * 0.1,
                       self.max_feature_value * 0.01,
                       # point of expense:
                       self.max_feature_value * 0.001,
                       ]
 
-        
-        
         # extremely expensive
         b_range_multiple = 2
+        
         # we dont need to take as small of steps
         # with b as we do w
         b_multiple = 5
+        
+        # Helps speed up convergence
         latest_optimum = self.max_feature_value*10
         
+        # Begin step process
         for step in step_sizes:
             w = np.array([latest_optimum,latest_optimum])
+            
             # we can do this because convex
+            # Optimized until no more steps
             optimized = False
+            
+            # 
             while not optimized:
+                
+                # Iterate through b (max b)
                 for b in np.arange(-1*(self.max_feature_value*b_range_multiple),
                                    self.max_feature_value*b_range_multiple,
                                    step*b_multiple):
                     for transformation in transforms:
+                        
+                        # Transform various weights
                         w_t = w*transformation
                         found_option = True
                         # weakest link in the SVM fundamentally
                         # SMO attempts to fix this a bit
+                        # Constraint function
                         # yi(xi.w+b) >= 1
                         # 
-                        # #### add a break here later..
+                        # #### add a break here later..\
+                        
+                        
                         for i in self.data:
                             for xi in self.data[i]:
                                 yi=i
+                                
+                                # Constraint is not true, then throw all out
                                 if not yi*(np.dot(w_t,xi)+b) >= 1:
                                     found_option = False
                                     #print(xi,':',yi*(np.dot(w_t,xi)+b))
                                     
                         if found_option:
+                            
+                            # Get euclidean distance if option found
                             opt_dict[np.linalg.norm(w_t)] = [w_t,b]
 
+                # Check for optimal step
                 if w[0] < 0:
                     optimized = True
                     print('Optimized a step.')
+
+                # Otherwise, take a step.    
                 else:
                     w = w - step
-
+            
+            # Sorting list of magnitudes
             norms = sorted([n for n in opt_dict])
             #||w|| : [w,b]
+            
+            # Get optimal choice
             opt_choice = opt_dict[norms[0]]
+
+            # Get w and b
             self.w = opt_choice[0]
             self.b = opt_choice[1]
+
+            # Reset current optimum for current steps
             latest_optimum = opt_choice[0][0]+step*2
             
         for i in self.data:
@@ -97,6 +131,7 @@ class Support_Vector_Machine:
                 print(xi,':',yi*(np.dot(self.w,xi)+self.b))            
 
     def predict(self,features):
+        # Classification prediction returns the sign of the margin where x %*% w + b
         # sign( x.w+b )
         classification = np.sign(np.dot(np.array(features),self.w)+self.b)
         if classification !=0 and self.visualization:
@@ -112,6 +147,8 @@ class Support_Vector_Machine:
         # nsv = -1
         # dec = 0
         def hyperplane(x,w,b,v):
+            
+            # Return SV value
             return (-w[0]*x-b+v) / w[1]
 
         datarange = (self.min_feature_value*0.9,self.max_feature_value*1.1)
@@ -138,6 +175,7 @@ class Support_Vector_Machine:
 
         plt.show()
         
+# Create classifier data
 data_dict = {-1:np.array([[1,7],
                           [2,8],
                           [3,8],]),
@@ -162,3 +200,4 @@ for p in predict_us:
     svm.predict(p)
 
 svm.visualize()
+
